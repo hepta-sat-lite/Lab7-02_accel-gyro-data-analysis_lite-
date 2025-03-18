4d88eb242f85fb8713ed635f08bb61f143d68998
sonenagisa <93703736+sonenagisa@users.noreply.github.com>
2025-03-18 15:37:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ df.columns = ['time', 'battery_voltage', 'temperature',
               'accel_x', 'accel_y', 'accel_z', 
               'gyro_x', 'gyro_y', 'gyro_z', 
               'mag_x', 'mag_y', 'mag_z']
-
+plt.rcParams.update({'font.size': 30})  # ここで一括変更
 # 1つのグラフを作成
 plt.figure(figsize=(15, 10))
-plt.scatter(df['time'], df['battery_voltage'], s=10, color='blue')
+plt.scatter(df['time'], df['battery_voltage'], s=30, color='blue')
 plt.title('Time vs Battery Voltage')
 plt.xlabel('Time')
 plt.ylabel('Battery Voltage [V]')
